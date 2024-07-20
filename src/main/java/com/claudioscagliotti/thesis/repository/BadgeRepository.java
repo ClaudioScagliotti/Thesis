59915d8f5ee33480f5690216ce2d9ffe5ae5d634
ClaudioScagliotti <cscagliotti99@gmail.com>
2024-07-20 10:54:19 +0200

    feat: modified checkBadgeForUser method, added badge level function

--- src/main/java/com/claudioscagliotti/thesis/repository/BadgeRepository.java
+++ src/main/java/com/claudioscagliotti/thesis/repository/BadgeRepository.java
@@ -1,13 +1,19 @@
 package com.claudioscagliotti.thesis.repository;
 
 import com.claudioscagliotti.thesis.model.BadgeEntity;
+import com.claudioscagliotti.thesis.model.GenreEntity;
 import com.claudioscagliotti.thesis.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BadgeRepository extends JpaRepository<BadgeEntity, Long> {
-    public List<BadgeEntity> findByUserEntityListContaining(UserEntity userEntity);
+    List<BadgeEntity> findByUserEntityListContaining(UserEntity userEntity);
+    @Query("SELECT COUNT(b) FROM BadgeEntity b JOIN b.userEntityList u WHERE u.id = :userId")
+    Integer countByUserId(@Param("userId") Long userId);
+    List<BadgeEntity> findByGenreToUnlock(GenreEntity genreEntity);
 }
--- src/main/java/com/claudioscagliotti/thesis/service/BadgeService.java
+++ src/main/java/com/claudioscagliotti/thesis/service/BadgeService.java
@@ -3,6 +3,7 @@ package com.claudioscagliotti.thesis.service;
 import com.claudioscagliotti.thesis.dto.response.BadgeDto;
 import com.claudioscagliotti.thesis.mapper.BadgeMapper;
 import com.claudioscagliotti.thesis.model.BadgeEntity;
+import com.claudioscagliotti.thesis.model.GenreEntity;
 import com.claudioscagliotti.thesis.model.UserEntity;
 import com.claudioscagliotti.thesis.repository.BadgeRepository;
 import jakarta.persistence.EntityExistsException;
@@ -10,6 +11,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -26,6 +28,7 @@ public class BadgeService {
     private final BadgeMapper badgeMapper;
     private final UserService userService;
     private final UserStatsService userStatsService;
+    private final GenreService genreService;
 
     /**
      * Constructs a BadgeService with required dependencies.
@@ -34,12 +37,14 @@ public class BadgeService {
      * @param badgeMapper      The mapper for converting between BadgeEntity and DTOs.
      * @param userService      The service for user-related operations.
      * @param userStatsService The service to manage userStatistics
+     * @param genreService     The service to manage Genre
      */
-    public BadgeService(BadgeRepository badgeRepository, BadgeMapper badgeMapper, UserService userService, UserStatsService userStatsService) {
+    public BadgeService(BadgeRepository badgeRepository, BadgeMapper badgeMapper, UserService userService, UserStatsService userStatsService, GenreService genreService) {
         this.badgeRepository = badgeRepository;
         this.badgeMapper = badgeMapper;
         this.userService = userService;
         this.userStatsService = userStatsService;
+        this.genreService = genreService;
     }
 
     /**
@@ -71,8 +76,6 @@ public class BadgeService {
         if (!userEntity.getBadgeEntitySet().contains(badgeEntity)) {
             userEntity.getBadgeEntitySet().add(badgeEntity);
             userService.saveUser(userEntity);
-        } else {
-            throw new EntityExistsException("User already has this badge.");
         }
 
         return badgeMapper.toBadgeDto(badgeEntity);
@@ -87,9 +90,24 @@ public class BadgeService {
         List<BadgeEntity> badgeEntityList = badgeRepository.findAll();
         return badgeMapper.toBadgeDto(badgeEntityList);
     }
+
+    /**
+     * Retrieves all badges by genreId.
+     *
+     * @param genreId The ID of the genre for which to retrieve badges.
+     * @return A list of BadgeDto objects representing all available badges for the specified genre.
+     */
+    public List<BadgeDto> getAllBadgeByGenreId(Long genreId) {
+        GenreEntity genreEntity = genreService.mapGenreIdsToEntities(Collections.singletonList(genreId)).stream().findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("There is no GenreEntity with id: " + genreId));
+
+        List<BadgeEntity> badgeEntityList = badgeRepository.findByGenreToUnlock(genreEntity);
+        return badgeMapper.toBadgeDto(badgeEntityList);
+    }
+
     /**
      * Checks if the user has accumulated enough movies in a specific genre to warrant a badge.
-     *
+     * <p>
      * This method retrieves the genre counts for the specified user and compares these counts to a predefined threshold
      * (defined by the {@code numberOfFilm} variable). If the number of movies for a genre exceeds this threshold, the method checks
      * if there is a badge associated with that genre. If a badge is found, it is assigned to the user.
@@ -100,21 +118,34 @@ public class BadgeService {
     public void checkBadgeForUser(UserEntity user) {
         Map<Long, Long> genreCountByUserId = userStatsService.getGenreCountByUserId(user.getId());
 
-        List<BadgeDto> allBadges = getAllBadge();
-
         for (Map.Entry<Long, Long> entry : genreCountByUserId.entrySet()) {
             Long genreId = entry.getKey();
             Long count = entry.getValue();
 
 
-            if (count > numberOfFilm) {
+            List<BadgeDto> allBadgeByGenreId = getAllBadgeByGenreId(genreId);
 
-                allBadges.stream()
-                        .filter(badgeDto -> badgeDto.getGenreToUnlock() != null
-                                && badgeDto.getGenreToUnlock().equals(genreId))
-                        .forEach(badgeDto -> addBadgeForUser(user.getUsername(), badgeDto.getId()));
+            for (BadgeDto badgeDto : allBadgeByGenreId) {
+                if (badgeDto.getGenreToUnlock() != null && badgeDto.getGenreToUnlock().equals(genreId)) {
+                    Float badgeLevel = badgeDto.getLevel();
+
+                    if (count > numberOfFilm * badgeLevel) {
+                        addBadgeForUser(user.getUsername(), badgeDto.getId());
+                    }
+                }
             }
         }
+    }
+
+
+    /**
+     * Retrieves the total count of badges for the specified user.
+     *
+     * @param userId the ID of the user
+     * @return the total count of badges for the user
+     */
+    public Integer getBadgeCountByUserId(Long userId) {
+        return badgeRepository.countByUserId(userId);
     }
 }
 
