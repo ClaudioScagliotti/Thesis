d0a9e4dd75f9c4c1daf129b8257a3743043b791a
ClaudioScagliotti <cscagliotti99@gmail.com>
2024-08-19 09:30:52 +0200

    test: added tests

--- src/test/java/com/claudioscagliotti/thesis/repository/ImageRepositoryTest.java
+++ src/test/java/com/claudioscagliotti/thesis/repository/ImageRepositoryTest.java
@@ -2,38 +2,86 @@ package com.claudioscagliotti.thesis.repository;
 
 import com.claudioscagliotti.thesis.ThesisApplication;
 import com.claudioscagliotti.thesis.model.ImageEntity;
+import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ThesisApplication.class)
 class ImageRepositoryTest {
 
     @Autowired
     private ImageRepository repository;
-
+    private ImageEntity newImage;
     @BeforeEach
     void setUp() {
+        newImage = new ImageEntity();
+        newImage.setUrl("https://example.com/image.jpg");
+        newImage.setDescription("A sample image");
+        newImage.setBase64Data("base64EncodedString");
+
+        ImageEntity savedImage = repository.save(newImage);
     }
 
     @AfterEach
     void tearDown() {
+        repository.delete(newImage);
     }
 
     @Test
-    public void testFindAll() {
+    void shouldSaveNewImage() {
+        ImageEntity newImage = new ImageEntity();
+        newImage.setUrl("https://example.com/image.jpg");
+        newImage.setDescription("A sample image");
+        newImage.setBase64Data("base64EncodedString");
+
+        ImageEntity savedImage = repository.save(newImage);
+
+        BDDAssertions.then(savedImage.getId()).isNotNull();
+        BDDAssertions.then(savedImage.getUrl()).isEqualTo("https://example.com/image.jpg");
+    }
+
+    @Test
+    void shouldFindAllImages() {
         List<ImageEntity> list = repository.findAll();
-        assertThat(list.size()).isEqualTo(2L);
+        BDDAssertions.then(list).isNotNull();
+        BDDAssertions.then(list.size()).isGreaterThanOrEqualTo(1);
+    }
+
+    @Test
+    void shouldDeleteImageById() {
+        ImageEntity imageToDelete = repository.findAll().get(0);
+        int size = repository.findAll().size();
+        repository.deleteById(imageToDelete.getId());
+
+        List<ImageEntity> remainingImages = repository.findAll();
+        BDDAssertions.then(remainingImages.size()).isEqualTo(size-1);
+        BDDAssertions.then(remainingImages.contains(imageToDelete)).isFalse();
+    }
+
+    @Test
+    void shouldUpdateImage() {
+        ImageEntity imageToUpdate = repository.findAll().get(0);
+        imageToUpdate.setDescription("Updated description");
+
+        ImageEntity updatedImage = repository.save(imageToUpdate);
+
+        BDDAssertions.then(updatedImage.getDescription()).isEqualTo("Updated description");
+    }
+
+    @Test
+    void shouldFindImageById() {
+        ImageEntity image = repository.findAll().get(0);
+        Optional<ImageEntity> foundImage = repository.findById(image.getId());
+
+        BDDAssertions.then(foundImage).isPresent();
+        BDDAssertions.then(foundImage.get().getId()).isEqualTo(image.getId());
     }
 }
--- src/test/java/com/claudioscagliotti/thesis/repository/KeywordRepositoryTest.java
+++ src/test/java/com/claudioscagliotti/thesis/repository/KeywordRepositoryTest.java
@@ -2,22 +2,21 @@ package com.claudioscagliotti.thesis.repository;
 
 import com.claudioscagliotti.thesis.ThesisApplication;
 import com.claudioscagliotti.thesis.model.KeywordEntity;
+import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ThesisApplication.class)
+@Transactional
 class KeywordRepositoryTest {
 
     @Autowired
@@ -29,12 +28,57 @@ class KeywordRepositoryTest {
 
     @AfterEach
     void tearDown() {
+        keywordRepository.deleteAll();
     }
 
     @Test
-    public void testFindAll() {
+    void shouldFindAllKeywords() {
         List<KeywordEntity> list = keywordRepository.findAll();
-        assertThat(list.size()).isEqualTo(2L);
+        BDDAssertions.then(list).isNotNull();
+        BDDAssertions.then(list.size()).isGreaterThan(0);
     }
 
+    @Test
+    void shouldSaveNewKeyword() {
+        KeywordEntity newKeyword = new KeywordEntity();
+        newKeyword.setName("New Keyword");
+
+        KeywordEntity savedKeyword = keywordRepository.save(newKeyword);
+
+        BDDAssertions.then(savedKeyword.getId()).isNotNull();
+        BDDAssertions.then(savedKeyword.getName()).isEqualTo("New Keyword");
+    }
+
+    @Test
+    void shouldDeleteKeywordById() {
+        KeywordEntity newKeyword = new KeywordEntity();
+        newKeyword.setName("New Keyword");
+
+        KeywordEntity keywordToDelete = keywordRepository.save(newKeyword);
+        int size = keywordRepository.findAll().size();
+        keywordRepository.deleteById(keywordToDelete.getId());
+
+        List<KeywordEntity> remainingKeywords = keywordRepository.findAll();
+        BDDAssertions.then(remainingKeywords.size()).isEqualTo(size-1);
+        BDDAssertions.then(remainingKeywords.contains(keywordToDelete)).isFalse();
+    }
+
+    @Test
+    void shouldUpdateKeyword() {
+        KeywordEntity keywordToUpdate = keywordRepository.findAll().get(0);
+        keywordToUpdate.setName("Updated Keyword");
+
+        KeywordEntity updatedKeyword = keywordRepository.save(keywordToUpdate);
+
+        BDDAssertions.then(updatedKeyword.getName()).isEqualTo("Updated Keyword");
+    }
+
+    @Test
+    void shouldFindKeywordById() {
+        KeywordEntity keyword = keywordRepository.findAll().get(0);
+        Optional<KeywordEntity> foundKeyword = keywordRepository.findById(keyword.getId());
+
+        BDDAssertions.then(foundKeyword).isPresent();
+        BDDAssertions.then(foundKeyword.get().getId()).isEqualTo(keyword.getId());
+    }
 }
